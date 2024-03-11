92aa93b58467cb1f1516d68c31a730b4d8c291c5
GuyD <124672797+GuyDuerFB@users.noreply.github.com>
2024-03-11 14:22:26 +0200

    Update update_docs.py

--- scripts/update_docs.py
+++ scripts/update_docs.py
@@ -23,12 +23,21 @@ def clone_source_repo():
     Repo.clone_from(SOURCE_REPO_URL, TEMP_CLONE_DIR, branch='gh-pages')
 
 def clean_content(content):
+<<<<<<< HEAD
     """Remove HTML tags, URLs, and Markdown headers from content, using BeautifulSoup for HTML."""
     soup = BeautifulSoup(content, 'lxml')  # 'html.parser' could be used as well
     text = soup.get_text(separator=' ', strip=True)
     text_no_urls = re.sub(r'http[s]?://\S+', '', text)  # Remove URLs
     text_no_md_headers = re.sub(r'#+\s?', '', text_no_urls)  # Remove Markdown headers
     return text_no_md_headers
+=======
+    """Remove HTML tags and URLs from content using BeautifulSoup."""
+    soup = BeautifulSoup(content, 'lxml')  # Use 'html.parser' if you don't want to use lxml
+    text = soup.get_text(separator=' ', strip=True)
+    # Remove URLs
+    text_no_urls = re.sub(r'http[s]?://\S+', '', text)
+    return text_no_urls
+>>>>>>> parent of 0a52346 (Update update_docs.py)
 
 def aggregate_docs_content():
     """Aggregates and cleans the content of all Markdown files, minimizing whitespace."""
@@ -41,7 +50,11 @@ def aggregate_docs_content():
                 with open(file_path, 'r', encoding='utf-8') as md_file:
                     file_content = md_file.read()
                     cleaned_content = clean_content(file_content)
+<<<<<<< HEAD
                     aggregated_content += cleaned_content.strip() + "\n"  # Minimize whitespace
+=======
+                    aggregated_content += cleaned_content + "\n\n"
+>>>>>>> parent of 0a52346 (Update update_docs.py)
     return aggregated_content
 
 def update_target_file(content):
